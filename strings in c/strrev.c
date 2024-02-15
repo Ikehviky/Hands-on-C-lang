a6e4017a35d22dd3732d0215590373b174c18250
Ikeh Victor <victorikeh77@gmail.com>
2024-02-15 09:29:26 -0500

    string reverse logic

---
+++ strings in c/strrev.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char ch;
+    char s1[30];
+    int i, len;
+
+    printf("Enter your string for reversing: \n");
+    fgets(s1, sizeof(s1), stdin);
+
+    
+
+    // len = strlen(s1);
+
+    // for(i = 0; i < len/2; i++)
+    // {
+    //     ch = s1[i];
+    //     s1[i] = s1[len-1-i];
+    //     s1[len-1-i] = ch;
+    // }
+
+    //     printf("%s\n", s1);
+
+    
+}
