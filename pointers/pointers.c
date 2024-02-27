ab0fac858994437510f2d83a5117a5864e1a7af7
Ikeh Victor <victorikeh77@gmail.com>
2024-02-27 04:28:32 -0500

    added some explanation

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -3,21 +3,30 @@
 
 int main()
 {
-    int a = 20, b = a, c;
+    int a = 20, b = 9, c;
     int *p, *q;
 
     p = &a;
     q = &b; 
     c = *p;
 
+    *p = 10;
+
+    //Accessing the values of the variables
     printf("Value of a %d\n", a);
     printf("Value of b %d\n", b);
-    printf("Value of a %d\n", *p);
-    printf("Value of a %d\n", *(&a));
-    printf("address of p %p\n", p);
-    printf("c = %d\n", c);
+
+    //Accessing the values from pointers
+    printf("Value of a from a pointer %d\n", *p);
+    printf("Value of a by *(&a) %d\n", *(&a));
+
+    //The address
+    printf("address of p  %p\n", &p);
+    printf(" value of p  %p\n", p);
     printf("address of a %p\n", &a);
     printf("address of b %p\n", &b);
     printf("address of c %p\n", &c);
+
+    printf("c = %d\n", c);
 
 }
